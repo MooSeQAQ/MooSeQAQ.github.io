d0e559a783c2bbc5a5d9bc1ef5fd09de515a492b
MooSeQAQ <3032362536@qq.com>
2024-07-07 16:30:11 +0800

    Site updated: 2024-07-07 16:30:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/cpp/","2024/07/06/hello-world/"];function toRandomPost(){
+var posts=["2024/07/06/hello-world/","2024/07/07/CPP-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
