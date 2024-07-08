c2c2dccd317fc4adb3eec4c69d7d46a39487e841
MooSeQAQ <3032362536@qq.com>
2024-07-08 16:08:04 +0800

    Site updated: 2024-07-08 16:08:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/21/DVWA01/","2024/03/17/HTML/","2024/03/20/Linux命令/","2024/04/12/Python入门/","2024/04/20/Python-Flask框架/","2024/03/24/SQL注入/","2024/03/22/MySQL/","2024/03/27/XSS/","2024/03/30/Rois冬令营题解/","2024/05/16/SSRF/","2024/03/16/hello-world/","2024/04/02/文件上传/","2024/07/08/校赛题解/"];function toRandomPost(){
+var posts=["2024/03/17/HTML/","2024/03/20/Linux命令/","2024/03/21/DVWA01/","2024/03/22/MySQL/","2024/04/20/Python-Flask框架/","2024/04/12/Python入门/","2024/03/24/SQL注入/","2024/03/30/Rois冬令营题解/","2024/05/16/SSRF/","2024/03/16/hello-world/","2024/03/27/XSS/","2024/07/08/校赛题解/","2024/04/02/文件上传/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
